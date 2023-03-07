7354cab196e99ef1b0aae0c9d94a486d808561cd
kersiancoder <maxi.heijo@gmail.com>
2023-03-07 10:58:14 -0300

    sqlite and places

--- src/store/place.slice.js
+++ src/store/place.slice.js
@@ -1,6 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { getPlaces, insertPlace } from "../db";
 import Place from "../models/places";
+import { URL_GEOCODING } from "../utils/maps";
 
 const initialState = {
   places: [],
@@ -12,16 +14,54 @@ const placeSlice = createSlice({
   reducers: {
     addPlace: (state, action) => {
       const newPlace = new Place(
-        Date.now().toString(),
+        action.payload.id.toString(),
         action.payload.title,
         action.payload.image,
+        action.payload.address,
         action.payload.coords
       );
       state.places.push(newPlace);
     },
+    setPlaces: (state, action) => {
+      state.places = action.payload;
+    },
   },
 });
 
-export const { addPlace } = placeSlice.actions;
+export const { addPlace, setPlaces } = placeSlice.actions;
+
+export const savePlace = (title, image, coords) => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(URL_GEOCODING(coords?.lat, coords?.lng));
+
+      if (!response.ok) {
+        throw new Error("No se ha podido conectar cone el servicio de geolocalización");
+      }
+
+      const data = await response.json();
+      if (!data.results) throw new Error("No se ha podido encontrar la dirección");
+
+      const address = data.results[0].formatted_address;
+      const result = await insertPlace(title, image, address, coords);
+      console.warn("result", result);
+      dispatch(addPlace({ id: result.insertId, title, image, address, coords }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const loadPlaces = () => {
+  return async (dispatch) => {
+    try {
+      const result = await getPlaces();
+      dispatch(setPlaces(result?.rows?._array));
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+};
 
 export default placeSlice.reducer;
